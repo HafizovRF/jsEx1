9bbe69fb660b994cb68da9df0747d883b03264e3
Ruslan <ruslan.hafizov@gmail.com>
2020-03-01 20:28:06 +0300

    example 2

---
+++ js.js
@@ -0,0 +1,27 @@
+
+
+for (let i = 1; i<8; i++){
+    if (i==6){
+        continue;
+    }
+   console.log(i);
+}
+
+let i=0;
+while(i < 7){
+    i++;
+    if (i==6){
+        continue;
+    }
+    console.log(i);
+}
+
+i=0;
+do {
+    i++;
+    if (i==6){
+        continue;
+    }
+    console.log(i);
+
+} while(i<7);

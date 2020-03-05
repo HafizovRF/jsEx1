76aa580f28c3a5e024200dd607a6fa1419f24bc7
Ruslan <ruslan.hafizov@gmail.com>
2020-03-05 14:37:33 +0300

    new exercise

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 
-
+/*
 for (let i = 1; i<8; i++){
     if (i==6){
         continue;
@@ -25,3 +25,42 @@ do {
     console.log(i);
 
 } while(i<7);
+*/
+/*
+let options = {
+    width: 1024,
+    height: 1024,
+    color: 'red'
+    //font: ['Time', 'bold'];
+};
+
+for (let key in options){
+    console.log("options["+key+"] = "+options[key]);
+}
+console.log(options);
+
+console.log(Object.keys(options));
+*/
+
+let arr=[1, 'findus', 'lala'];
+arr.push('common');
+arr.unshift('cococ');
+let xci= arr.join(',');
+console.log(xci);
+
+
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => { 
+    console.log(accumulator+" and " + currentValue); return accumulator + currentValue; 
+};
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+console.log('reduceRight(reducer)');
+console.log(array1.reduceRight(reducer));
+console.log('--------');
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // 1
 let money = 0;
 let time = '2020-03-31';
@@ -12,7 +13,7 @@ function getNumberPrompt(description, val, tryCount = 10) {
         if (!isNaN(newval)) {
             return (newval);
         }
-        NumberStr = prompt('Не получилось распознать числовое значение.  '+'Попытка №'+String(i+2)+'. '+description, String(val));
+        NumberStr = prompt('Не получилось распознать числовое значение.  ' + 'Попытка №' + String(i + 2) + '. ' + description, String(val));
     }
     return (null);
 }
@@ -24,7 +25,7 @@ function getDatePrompt(description, val, tryCount = 10) {
         if (newval != null) {
             return (newval);
         }
-        timeStr = prompt('Не получилось распознать дату. Попытка №'+String(i+2)+'. '+description, String(val));
+        timeStr = prompt('Не получилось распознать дату. Попытка №' + String(i + 2) + '. ' + description, String(val));
     }
     return (1);
 }
@@ -34,20 +35,20 @@ time = getDatePrompt('Введите дату в формате YYYY-MM-DD', '20
 
 
 // 2
-let appData ={}
-if(money>0 && time!=null){
+let appData = {}
+if (money > 0 && time != null) {
 
     appData = {
         budget: money,
-        timeData:time,
-        expenses:{},
-        optionalExpenses:{},
-        income:[],
-        savings:false
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: false
     };
 
-    let finSt=['Девченки', 'Печеньки'];
-    for(let i=0; i<2; i++){
+    let finSt = ['Девченки', 'Печеньки'];
+    for (let i = 0; i < 2; i++) {
         let stName = prompt('Введите обязательную статью расходов в этом месяце', finSt[i]);
         let stVal = getNumberPrompt('Во сколько обойдется?', 100, 10);
         appData.expenses[stName] = stVal;
@@ -57,12 +58,12 @@ if(money>0 && time!=null){
 }
 
 
-let strMes='Общий бюджет на 1 день: '+String((appData.budget/30).toFixed(2))+'\n';
-    
+let strMes = 'Общий бюджет на 1 день: ' + String((appData.budget / 30).toFixed(2)) + '\n';
+
 for (const key in appData.expenses) {
     if (appData.expenses.hasOwnProperty(key)) {
         const element = appData.expenses[key];
-        strMes = strMes+'   ['+key+']: '+(element/30).toFixed(2)+'\n';
+        strMes = strMes + '   [' + key + ']: ' + (element / 30).toFixed(2) + '\n';
     }
 }
 
@@ -73,4 +74,66 @@ console.log(strMes);
 
 console.log('money: ' + String(money));
 console.log('time: ' + String(time));
-console.log('appdata:'+JSON.stringify(appData));
+console.log('appdata:' + JSON.stringify(appData));
+*/
+
+// 4 LESSON
+let appData = {
+    budget: 0,
+    timeData: '2020-03-31',
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+
+    getNumberPrompt: function (description, val, tryCount = 10) {
+        var NumberStr = prompt(description, String(val));
+
+        for (let i = 0; i < tryCount; i++) {
+            let newval = parseInt(NumberStr, 10);
+            if (!isNaN(newval)) {
+                return (newval);
+            }
+            NumberStr = prompt('Не получилось распознать числовое значение.  ' + 'Попытка №' + String(i + 2) + '. ' + description, String(val));
+        }
+        return (null);
+    },
+    getDatePrompt: function (description, val, tryCount = 10) {
+        let timeStr = prompt(description, String(val));
+        for (let i = 0; i < tryCount; i++) {
+            let newval = new Date(timeStr);
+            if (newval != null) {
+                return (newval);
+            }
+            timeStr = prompt('Не получилось распознать дату. Попытка №' + String(i + 2) + '. ' + description, String(val));
+        }
+        return (1);
+    },
+    calcBudget1day: function(){
+        this.budgetPerDay = (this.budget / 30);
+        return this.budgetPerDay;
+    },
+    initdata: function () {
+        this.budget = this.getNumberPrompt('Ваш бюджет в месяц', '10000', 10);
+        this.timeData = this.getDatePrompt('Введите дату в формате YYYY-MM-DD', '2020-02-21', 10);
+        this.calcBudget1day();
+    },
+    chooseExpenses:function(){
+        let finSt = ['Девченки', 'Печеньки'];
+        for (let i = 0; i < 2; i++) {
+            let stName = prompt('Введите обязательную статью расходов в этом месяце', finSt[i]);
+            let stVal = this.getNumberPrompt('Во сколько обойдется?', 100, 10);
+            this.expenses[stName] = stVal;
+        }
+    },
+    printExpensesPerDay: function(){
+        let strMes = this.budgetPerDay;
+        for (const key in this.expenses) {
+            if (this.expenses.hasOwnProperty(key)) {
+                const element = this.expenses[key];
+                strMes = strMes + '   [' + key + ']: ' + (element / 30).toFixed(2) + '\n';
+            }
+        }
+        console.log(strMes);
+    }
+};

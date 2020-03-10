e24ac687b6600ea17a1c5fc987e6195ce2b56996
Ruslan <ruslan.hafizov@gmail.com>
2020-03-10 21:28:57 +0300

    Commit by exercise 10

--- ex91/js/script.js
+++ ex91/js/script.js
@@ -1,5 +1,4 @@
-
-window.addEventListener('DOMContentLoaded', function(event){
+window.addEventListener('DOMContentLoaded', function (event) {
     'use strict';
 
     let tabControl = document.querySelector('.info'),
@@ -7,26 +6,26 @@ window.addEventListener('DOMContentLoaded', function(event){
         tabBtns = document.querySelectorAll('.info-header-tab'),
         tabs = document.querySelectorAll('.info-tabcontent');
 
-    function activateTab(ind){
-        for(let i=0; i<tabs.length; i++){
-            if(i==ind){
-                if(!tabs[i].classList.contains('show')){ 
+    function activateTab(ind) {
+        for (let i = 0; i < tabs.length; i++) {
+            if (i == ind) {
+                if (!tabs[i].classList.contains('show')) {
                     tabs[i].classList.add('show');
                     tabs[i].classList.remove('hide');
                 }
-            } else {                
-                if(!tabs[i].classList.contains('hide')) {
+            } else {
+                if (!tabs[i].classList.contains('hide')) {
                     tabs[i].classList.remove('show');
                     tabs[i].classList.add('hide');
                 }
             }
         }
     }
-    tabHead.addEventListener('click', function(event){
-        let target =event.target;
-        if(target && target.classList.contains('info-header-tab')){
-            for(let i = 0; i<tabBtns.length; i++){
-                if (tabBtns[i]==target){
+    tabHead.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tabBtns.length; i++) {
+                if (tabBtns[i] == target) {
                     activateTab(i);
                     break;
                 }
@@ -34,5 +33,44 @@ window.addEventListener('DOMContentLoaded', function(event){
         }
     });
 
+
+    function getTimerData(deadline) {
+        let ts = Date.parse(deadline) - new Date();
+        return {
+            total: ts,
+            hours: (Math.floor(ts / (1000 * 60 * 60) % 60)),
+            minutes: (Math.floor(ts / (1000 * 60) % 60)),
+            seconds: (Math.floor(ts / 1000) % 60)
+        };
+    }
+
+    function setTimer(id, deadline) {
+        let timerElm = document.getElementById(id),
+            hours = timerElm.querySelector('.hours'),
+            minutes = timerElm.querySelector('.minutes'),
+            seconds = timerElm.querySelector('.seconds'),
+            hndintervaler = setInterval(updateTimer, 1000);
+
+        function updateTimer() {
+            let time = getTimerData(deadline);
+
+            if (time.total < 0) {
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                clearInterval(hndintervaler);
+            } else {
+                hours.textContent = String(time.hours).padStart(2, 0);
+                minutes.textContent = String(time.minutes).padStart(2, 0);
+                seconds.textContent = String(time.seconds).padStart(2, 0);
+            }
+        }
+
+    }
+
+
     activateTab(0);
+
+    setTimer('timer', '2020-03-10 21:16:00');
+
 });
